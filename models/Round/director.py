fef53e1a72fcd3ed5a4935f196500a2c0e9a4cfc
PaoloVeliz <paoloveliz55@gmail.com>
2022-07-03 11:38:19 -0600

    Builder pattern almost done, waiting for the enemy abstract facotry to continue some parts

---
+++ models/Round/director.py
@@ -0,0 +1,11 @@
+from builder import Builder
+
+class Director():
+    def __init__(self, builder: Builder):
+        self.__buislder: Builder = builder
+
+    def change_builder(builder: Builder):
+        pass
+
+    def make(type: int):
+        pass
