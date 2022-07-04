1648ae5ba9e6706fcea662ea82138b50b9566155
PaoloVeliz <paoloveliz55@gmail.com>
2022-07-04 17:48:49 -0500

    the enemy can hit the player also, we fixed the bullet removing part

--- models/game.py
+++ models/game.py
@@ -1,3 +1,4 @@
+import logging
 import pygame
 # from game_object import Bullet, Player
 # from game_object.enemy import Enemy, Crazy
@@ -87,6 +88,11 @@ class Game:
                     if not enemy.tracking:
                         enemy.track()
 
+                    if enemy.x + enemy.hitbox.width >= player.x >= enemy.x - player.hitbox.width and \
+                            enemy.y + enemy.hitbox.height >= player.y >= enemy.y - player.hitbox.height:
+                        self.__screen_enemies.remove(enemy)
+                        
+
             if len(self.__screen_bullets) > 0:
                 for bullet in self.__screen_bullets:
                     bullet.shoot()
@@ -96,7 +102,13 @@ class Game:
                             bullet.y > self.__height or \
                             bullet.x < -player.hitbox.right or\
                             bullet.x > self.__width:
-                        self.__screen_bullets.remove(bullet)
+                        try:
+                            self.__screen_bullets.remove(bullet)
+                        except:
+                            logging.WARNING(f"error deleting bullet, maybe theres no bullet anymore :)")
+
+                            
+                        
 
                     for enemy in self.__screen_enemies:
                         # 50?
